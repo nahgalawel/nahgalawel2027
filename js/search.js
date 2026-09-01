c8c2e2b2bc3fcdcf311ef6ab711ee7720cefdd73
nahgalawel <110112588+nahgalawel@users.noreply.github.com>
2026-09-01 18:17:55 +0300

    Refactor search_exam function to use async/await

--- js/search.js
+++ js/search.js
@@ -1,16 +1,60 @@
-function search_exam() { num = $('#num_exam')['val'](); if (num != '') { $('#load')['css']('display', 'inline-block');
-        $['ajax']({ type: 'GET', url: 'https://app-exams.com/new_exam/get_exam_data2.php?id=' + parseArabic(num), success: function(_0x5ab8x2) { obj = JSON['parse'](_0x5ab8x2); if (obj['exam_data'] == false || obj['exam_data'] == 'false') { Swal['fire']({ type: 'info', html: lan_There_is_no_exam_number, confirmButtonText: lan_ok });
-                    $('#load')['css']('display', 'none') } else { if (obj['exam_name'] == '' && obj['exam_info'] == '' && obj['exam_email'] == '' && obj['exam_data'] == '') { get_prev_exam(num, obj['t_pass'], 'notYut', 'forUpdate') } else { exam_name = document['getElementById']('showName')['innerHTML'] = obj['exam_name'];
-                        exam_info = document['getElementById']('showNobza')['innerHTML'] = obj['exam_info'];
-                        exam_data = obj['exam_data'];
-                        myObj = JSON['parse'](exam_data); if (myObj['allow_download'] == 'yes') { if (getFromHash == 'yes') { setTimeout(function() { go_page('page_home');
-                                    downloadExam_new() }, 800) } else { $('#show_search')['hide']();
-                                $('#result_search')['show']();
-                                $('#load')['css']('display', 'none') } } else { Swal['fire']({ type: 'warning', html: lan_Downloading_not_allowed_at_this_time, confirmButtonText: lan_ok });
-                            $('#load')['css']('display', 'none') } } } }, error: function() { Swal['fire']({ type: 'info', html: lan_There_is_problem_connecting_network, showConfirmButton: false, timer: 1500 });
-                $('#load')['css']('display', 'none') } }) } else { Swal['fire']({ type: 'info', html: lan_Please_enter_exam_number, confirmButtonText: lan_ok }) } }
+async function search_exam() {
+    var num_exam = $('#num_exam').val();
+    if (!num_exam) {
+        alert('الرجاء إدخال رقم الاختبار');
+        return;
+    }
 
-function show_search() { $('#result_search')['hide']();
-    $('#show_search')['show']();
-    $('#btnDown')['parent']()['css']('display', 'table-cell');
-    $('#btnDown')['parent']()['next']()['attr']('colspan', 1) }
+    $('#load').show();
+
+    let { data, error } = await window._supabase
+        .from('exams')
+        .select('*')
+        .eq('exam_number', Number(num_exam))
+        .single();
+
+    $('#load').hide();
+
+    if (error || !data) {
+        alert('لم يتم العثور على الاختبار، تأكد من صحة الرقم.');
+        $('#result_search').addClass('Dnone');
+        return;
+    }
+
+    window.currentLoadedExam = data;
+
+    $('#showName').text(data.exam_name);
+    $('#showNobza').text(data.exam_info || 'لا توجد نبذة وصفية');
+    $('#show_search').addClass('Dnone');
+    $('#result_search').removeClass('Dnone');
+}
+
+function show_search() {
+    $('#result_search').addClass('Dnone');
+    $('#show_search').removeClass('Dnone');
+    $('#num_exam').val('');
+}
+
+function downloadExam_new() {
+    if (!window.currentLoadedExam) {
+        alert('الرجاء البحث عن اختبار أولاً');
+        return;
+    }
+
+    var exam = window.currentLoadedExam;
+    
+    let savedExams = JSON.parse(localStorage.getItem('downloaded_exams') || '[]');
+    let exists = savedExams.some(e => e.exam_number === exam.exam_number);
+    
+    if (!exists) {
+        savedExams.push(exam);
+        localStorage.setItem('downloaded_exams', JSON.stringify(savedExams));
+    }
+
+    if (typeof readAll_ans_saveded_new === 'function') {
+        readAll_ans_saveded_new();
+    }
+
+    // فتح وبدء الاختبار فوراً للطالب
+    startDownloadedExam(exam.exam_number);
+}
